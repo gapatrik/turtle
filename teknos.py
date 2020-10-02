5dd4146c2deef0cd1c249cffae994d137a5ff336
gapatrik <71643273+gapatrik@users.noreply.github.com>
2020-10-02 14:08:25 +0200

    Create teknos.py

---
+++ teknos.py
@@ -0,0 +1,17 @@
+import turtle               
+anlak= turtle.Screen()        
+alex = turtle.Turtle() 
+
+def rajz(szogek_szama, szin):         
+    hossz =100                 
+   
+    alex.color(szin)
+    for i in range(szogek_szama):
+        alex.forward(hossz)
+        alex.left(360/szogek_szama)
+        
+rajz(3, 'purple')      
+rajz(4,  'red')      
+rajz(5, 'green')
+rajz(6, 'orange')   
+    
